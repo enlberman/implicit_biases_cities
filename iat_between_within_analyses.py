cc5ce7f840ef6eb0df7fd18a6e1f6bc2a3c75534
andrewstier <andrewstier@uchicago.edu>
2023-09-05 09:38:49 -0500

    Update iat_between_within_analyses.py

--- iat_between_within_analyses.py
+++ iat_between_within_analyses.py
@@ -9,6 +9,7 @@ from os.path import join
 STATE_FIPS = {"AL":"01","AK":"02","AZ":"04","AR":"05","CA":"06","CO":"08","CT":"09","DE":"10","DC":"11","FL":"12","GA":"13","HI":"15","ID":"16","IL":"17","IN":"18","IA":"19","KS":"20","KY":"21","LA":"22","ME":"23","MD":"24","MA":"25","MI":"26","MN":"27","MS":"28","MO":"29","MT":"30","NE":"31","NV":"32","NH":"33","NJ":"34","NM":"35","NY":"36","NC":"37","ND":"38","OH":"39","OK":"40","OR":"41","PA":"42","RI":"44","SC":"45","SD":"46","TN":"47","TX":"48","UT":"49","VT":"50","VA":"51","WA":"53","WV":"54","WI":"55","WY":"56","AS":"60","GU":"66","MP":"69","PR":"72","VI":"78"}
 
 data_path = 'IAT_data'
+figure_path = 'IAT_figures'
 
 iat_data = pandas.read_csv(join(data_path,'cbsa_iat.csv'))
 
@@ -101,7 +102,7 @@ plt.violinplot([numpy.vstack(ff1[0][i])[:,1]-numpy.vstack(ff2[0][i])[:,1] for i 
 plt.ylabel(r'$\Delta\ AIC$')
 plt.xticks([1,6,11],[2010,2015,2020])
 plt.tight_layout()
-plt.savefig(join(data_path,'between_within_delta_aic.png'),dpi=300)
+plt.savefig(join(figure_path,'between_within_delta_aic.png'),dpi=300)
 plt.clf()
 plt.axhline(0,color='k')
 plt.violinplot([numpy.vstack(ff1[0][i])[:,0]-numpy.vstack(ff2[0][i])[:,0] for i in range(11)],showextrema=False,
@@ -109,7 +110,7 @@ plt.violinplot([numpy.vstack(ff1[0][i])[:,0]-numpy.vstack(ff2[0][i])[:,0] for i 
 plt.ylabel(r'$\Delta\ R^2$')
 plt.xticks([1,6,11],[2010,2015,2020])
 plt.tight_layout()
-plt.savefig(join(data_path,'between_within_delta_r2.png'),dpi=300)
+plt.savefig(join(figure_path,'between_within_delta_r2.png'),dpi=300)
 
 plt.clf()
 plt.axhline(0,color='k')
@@ -118,7 +119,7 @@ plt.violinplot([numpy.vstack(ff4[0][i])[:,1]-numpy.vstack(ff3[0][i])[:,1] for i 
 plt.ylabel(r'$\Delta\ AIC$')
 plt.xticks([1,6,11],[2010,2015,2020])
 plt.tight_layout()
-plt.savefig(join(data_path,'between_within_delta_aic_full.png'),dpi=300)
+plt.savefig(join(figure_path,'between_within_delta_aic_full.png'),dpi=300)
 plt.clf()
 plt.axhline(0,color='k')
 plt.violinplot([numpy.vstack(ff4[0][i])[:,0]-numpy.vstack(ff3[0][i])[:,0] for i in range(11)],showextrema=False,
@@ -126,4 +127,4 @@ plt.violinplot([numpy.vstack(ff4[0][i])[:,0]-numpy.vstack(ff3[0][i])[:,0] for i 
 plt.ylabel(r'$\Delta\ R^2$')
 plt.xticks([1,6,11],[2010,2015,2020])
 plt.tight_layout()
-plt.savefig(join(data_path,'between_within_delta_r2_full.png'),dpi=300)
+plt.savefig(join(figure_path,'between_within_delta_r2_full.png'),dpi=300)
